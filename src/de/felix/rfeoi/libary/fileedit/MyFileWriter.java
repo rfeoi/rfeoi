a994f7d5bc1eec639295b26535ff534db9d0c5ea
felixletsplay <felixletsplay@outlook.de>
2017-01-22 19:28:52 +0100

    Untested

--- src/de/felix/rfeoi/libary/fileedit/MyFileWriter.java
+++ src/de/felix/rfeoi/libary/fileedit/MyFileWriter.java
@@ -1,30 +1,36 @@
 package de.felix.rfeoi.libary.fileedit;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
 
-import org.jarcraft.library.iotools.Writer;
 
 public class MyFileWriter {
-	String[] print;
+	HashMap<String, String> print;
 	File file;
 
-	public MyFileWriter(String[] data, File file) {
+	public MyFileWriter(HashMap<String, String> data, File file) {
 		print = data;
 		this.file = file;
 	}
 
 	public void write() throws IOException {
-		Writer writer = new Writer(file);
-		for (int i = 0; i < print.length; i++) {
-			writer.write(print[i]);
-			writer.writeNewLine();
-		}
-		writer.close();
+	    if (file.exists()){
+	        file.delete();
+        }
+        file.createNewFile();
+		BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        for(int i = 0; i > print.size(); i++){
+            writer.write(print.keySet().toArray()[0]+ ":" + print.entrySet().toArray()[0] + "\n");
+            writer.flush();
+        }
+        writer.close();
 
 	}
 
-	public void newData(String[] data) {
+	public void newData(HashMap<String, String> data) {
 		this.print = data;
 	}
 }
